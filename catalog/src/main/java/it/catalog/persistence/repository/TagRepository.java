de9a2097ee137d0429d400d689a2a5e47d866949
giuseppe.parisi@naoslab.it <giuseppe.parisi@naoslab.it>
2026-03-17 14:50:42 +0100

    Cambiata la query di recupero dei Tag per tipo di oggetto

--- catalog/src/main/java/it/catalog/persistence/repository/TagRepository.java
+++ catalog/src/main/java/it/catalog/persistence/repository/TagRepository.java
@@ -4,23 +4,19 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import it.catalog.persistence.entity.Tag;
 
-public interface TagRepository extends JpaRepository<Tag, Long>, JpaSpecificationExecutor<Tag> {
+public interface TagRepository extends JpaRepository<Tag, Long> {
     
 	Optional<Tag> findByNomeTag(String nomeTag);
 	List<Tag> findByTipoOggetto(String tipoOggetto);
 	
 	
-	 @Query("""
-	            SELECT t FROM OggettoTag ot
-	            JOIN ot.tag t
-	            WHERE t.tipoOggetto = :tipoOggetto AND t.idTag =ot.tag.idTag and ot.idOggetto=:idOggetto
-	        """)
-	List<Tag> findByTipoOggettoAndIdOggetto(String tipoOggetto, Long idOggetto);
+	 @Query(value="SELECT t.* FROM tag t left JOIN Oggetto_tag ot on t.id_tag =ot.id_tag where t.tipo_Oggetto = :tipoOggetto and ot.id_oggetto=:idOggetto",nativeQuery = true)
+	List<Tag> findByTipoOggettoAndIdOggetto(@Param("tipoOggetto") String tipoOggetto,  @Param("idOggetto")Long idOggetto);
 
 	 
 //	 List<Tag> findByTipoOggettoAndTags(String tipoOggetto, List<String> tagNames);
