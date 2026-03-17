4b1ae6aea6dd7b9c3147f4691ff07b13830d0ab8
giuseppe.parisi@naoslab.it <giuseppe.parisi@naoslab.it>
2026-03-17 15:06:16 +0100

    Cambio dei service findPage(), count() e save()

--- catalog/src/main/java/it/catalog/service/impl/AudioFileServiceImpl.java
+++ catalog/src/main/java/it/catalog/service/impl/AudioFileServiceImpl.java
@@ -2,13 +2,13 @@ package it.catalog.service.impl;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import it.catalog.persistence.entity.AudioFile;
 import it.catalog.persistence.repository.AudioRepository;
@@ -46,16 +46,17 @@ public class AudioFileServiceImpl implements SearchService<AudioDto, DtoFilter> 
 	}
 
 	@Override
-	public Page<AudioDto> findPage(DtoFilter filtro, Pageable pageable) {
+	 @Transactional(readOnly = true)
+	public Page<AudioDto> findPage(Pageable pageable,DtoFilter filter) {
 
-		Specification<AudioFile> spec = (root, query, cb) -> cb.conjunction();
+		//Specification<AudioFile> spec = (root, query, cb) -> cb.conjunction();
 
 //		if (filtro != null && filtro.getCriterion() != null) {
 //            try {
 //           	 Field field = AudioDto.class.getDeclaredField(filtro.getCampo());
 //                Class<?> type = field.getType();
 
-			spec = specFactory.build("Audio", filtro);
+		//	spec = specFactory.build("Audio", filtro);
 			// 2) Tag filter
 //          List<String> requiredTags = (filtro.getTags() != null && !filtro.getTags().isEmpty()) ? 
 //        		  filtro.getTags().stream().map(TagDto::getNomeTag).collect(Collectors.toList()): 
@@ -70,12 +71,34 @@ public class AudioFileServiceImpl implements SearchService<AudioDto, DtoFilter> 
 //            }
 //		}
 
-//		Page<AudioFile> entityPage = repo.findAll(spec, pageable);
-		Page<AudioFile> entityPage = repo.findAll(pageable);
+		//Page<AudioFile> entityPage = repo.findAll(spec, pageable);
+		
+	    // Verifichiamo se la dimensione richiesta è superiore a 25
+//	    int pageSize = Math.min(pageable.getPageSize(), 25);
+//	    
+//	    // Creiamo un nuovo PageRequest basato sui parametri originali ma col limite di 25
+//	    Pageable constrainedPageable = PageRequest.of(
+//	            pageable.getPageNumber(), 
+//	            pageSize, 
+//	            pageable.getSort()
+//	    );
+
+//		Page<AudioFile> entityPage = repo.findAllAudio(pageable);
+		
+		
+	    // 1. Creiamo la specifica basata sul filtro ricevuto dalla UI
+	    Specification<AudioFile> spec = specFactory.build("Audio", filter);
+
+	 // 2. Eseguiamo la query filtrata e paginata
+	    // Grazie al @BatchSize(size=25) sull'entità, i tag verranno caricati efficientemente
+	    Page<AudioFile> entityPage = repo.findAll(spec, pageable);
+	    
 		if (entityPage.isEmpty()) { // caso Not Found
 			return new PageImpl<>(Collections.emptyList(), pageable, 0); 
 		}
-		
+		// inizializza i tag per ogni AudioFile
+//		entityPage.getContent().forEach(a -> a.getTags().size());
+
 		return mapper.toDtoPage(entityPage);
 
 		/*
@@ -97,11 +120,11 @@ public class AudioFileServiceImpl implements SearchService<AudioDto, DtoFilter> 
 	}
 	
 	@Override
-	public long count(DtoFilter filter) {
-		Specification<AudioFile> spec = (root, query, cb) -> cb.conjunction();
+	public long count() {
 
-		spec = specFactory.build("Audio", filter);
-		return repo.count(spec);
+		return repo.count();		 
+//		 Page<AudioFileCustomerEntity> page=repo.findAllAudio(pageable);
+//		return page.getTotalElements();
 	}
 	
 
@@ -111,25 +134,33 @@ public class AudioFileServiceImpl implements SearchService<AudioDto, DtoFilter> 
 	 */
 	public AudioDto findById(Long id) {
 		var dtoOpt = repo.findById(id).map(mapper::toDto);
-		dtoOpt.ifPresent(dto -> dto.setTags(tagService.findTagsByObject("Audio", dto.getId())));
+//		dtoOpt.ifPresent(dto -> dto.setTags(tagService.findTagsByObject("Audio", dto.getId())));
 		return dtoOpt.orElse(new AudioDto());
 	}
 
 	@Override
+	@Transactional
 	public AudioDto save(AudioDto dto) {
-		AudioFile saved = repo.save(mapper.toEntity(dto));
+		
+		// 1. MapStruct crea l'entità (con il Set<Tag> popolato dai TagDto)
+        AudioFile entity = mapper.toEntity(dto);
+        
+        // 2. Hibernate salva. 
+        // - Se un Tag ha ID null, lo inserisce in 'tag' (grazie a PERSIST)
+        // - Se un Tag ha ID ma è stato modificato, lo aggiorna (grazie a MERGE)
+        // - In ogni caso, inserisce/aggiorna le righe in 'oggetto_tag'
+        
+		AudioFile saved = repo.save(entity);
 		log.info("Save New Audio file");
 		// Update tag relations
-		List<String> tagNames = dto.getTags() != null
-				? dto.getTags().stream().map(TagDto::getNomeTag).collect(Collectors.toList())
-				: Collections.EMPTY_LIST;
+//		List<String> tagNames = dto.getTags() != null
+//				? dto.getTags().stream().map(TagDto::getNomeTag).collect(Collectors.toList())
+//				: Collections.EMPTY_LIST;
 
-		tagService.upsertTagsForObject("Audio", saved.getId(), tagNames);
-		log.info("Update Tags list for Audio file {}", saved.getId());
+//		tagService.upsertTagsForObject("Audio", saved.getId(), tagNames);
+//		log.info("Update Tags list for Audio file {}", saved.getId());
 
-		AudioDto res = mapper.toDto(saved);
-		res.setTags(dto.getTags());
-		return res;
+		return mapper.toDto(saved);
 	}
 
 	@Override
