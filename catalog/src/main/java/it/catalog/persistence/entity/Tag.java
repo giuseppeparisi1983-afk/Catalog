e56f8b3091ddcf7efabf84c67a282b1a3d8a78bb
giuseppe.parisi@naoslab.it <giuseppe.parisi@naoslab.it>
2026-03-17 14:52:39 +0100

    Commit

--- catalog/src/main/java/it/catalog/persistence/entity/Tag.java
+++ catalog/src/main/java/it/catalog/persistence/entity/Tag.java
@@ -1,38 +1,44 @@
 package it.catalog.persistence.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
-@NoArgsConstructor //Hibernate (e in generale JPA) quando carica l’entità dal DB, richiede sempre un costruttore di default senza argomenti per poter istanziare le entity tramite riflessione.
+@NoArgsConstructor // Hibernate (e in generale JPA) quando carica l’entità dal DB, richiede sempre
+					// un costruttore di default senza argomenti per poter istanziare le entity
+					// tramite riflessione.
 @Table(name = "tag")
 public class Tag {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long idTag;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idTag;
 
-    @Column(nullable = false, unique = true)
-    private String nomeTag;
-    
-    @Column(name = "tipo_oggetto", nullable = false)
-  private String tipoOggetto;
-    
-    
-    public Tag(String nomeTag) { this.nomeTag = nomeTag; }
+	@Column(nullable = false, unique = true)
+	private String nomeTag;
 
+	@Column(name = "tipo_oggetto", nullable = false)
+	private String tipoOggetto;
+
+	public Tag(String nomeTag) {
+		this.nomeTag = nomeTag;
+	}
 
 	public Tag(String nomeTag, String tipoOggetto) {
 		super();
 		this.nomeTag = nomeTag;
 		this.tipoOggetto = tipoOggetto;
 	}
-    
-    
+
 }
