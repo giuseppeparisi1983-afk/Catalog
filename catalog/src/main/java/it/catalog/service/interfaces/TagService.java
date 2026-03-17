08020e7735709e9c0046d40bec92395b80100cc0
giuseppe.parisi@naoslab.it <giuseppe.parisi@naoslab.it>
2026-03-17 15:00:35 +0100

    rimozione delle query findObjectTagByObject() e upsertTagsForObject()

--- catalog/src/main/java/it/catalog/service/interfaces/TagService.java
+++ catalog/src/main/java/it/catalog/service/interfaces/TagService.java
@@ -1,17 +1,15 @@
 package it.catalog.service.interfaces;
 
 import java.util.List;
-import java.util.Optional;
 
-import it.catalog.persistence.entity.OggettoTag;
 import it.catalog.service.dto.TagDto;
 
 public interface TagService {
     
 	public List<TagDto> findTagsByObject(String tipoOggetto, Long idOggetto);
-	public List<OggettoTag> findObjectTagByObject(String tipoOggetto, Long idOggetto);
+//	public List<OggettoTag> findObjectTagByObject(String tipoOggetto, Long idOggetto);
 	//public void saveTagsForObject(String tipoOggetto, Long idOggetto, List<String> nomiTag);
-	public void upsertTagsForObject(String tipoOggetto, Long idOggetto, List<String> desiredTagNames);
+//	public void upsertTagsForObject(String tipoOggetto, Long idOggetto, List<String> desiredTagNames);
 	public List<TagDto> findByTipoOggetto(String tipo);
 //	TagDto create(TagDto dto);
 //    Optional<TagDto> findById(Long id);
