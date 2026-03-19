22853ef3e5e2aeb9c4ce3f87bab1c60456a53d0e
giuseppe.parisi@naoslab.it <giuseppe.parisi@naoslab.it>
2026-03-19 11:17:41 +0100

    Aggiunti i nuovi campi: preferito,rating,visualizzazioni e backup con le date gestite con Instant

--- catalog/src/main/java/it/catalog/service/dto/DocumentoDto.java
+++ catalog/src/main/java/it/catalog/service/dto/DocumentoDto.java
@@ -1,6 +1,6 @@
 package it.catalog.service.dto;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 import java.util.List;
 
 import it.catalog.common.enums.StatiDocumento;
@@ -16,9 +16,6 @@ public class DocumentoDto {
 	    private String nome;
 	    private String path;
 	    private Long dimensione;
-	    private LocalDateTime dataCreazione;
-	    private LocalDateTime lastUpdate;
-	    private LocalDateTime lastView;
 	    private String autore;
 	    private String descrizione;
 	    private TipoDocumento categoria;
@@ -26,8 +23,15 @@ public class DocumentoDto {
 	    private Integer versione;
 	    private StatiDocumento stato;
 	    private String origine;
+	    private boolean preferito; 
+	    private Integer rating; 
+	    private long visualizzazioni; 
+	    private boolean backup;
 	    private String note;
 	    private String estensione;
+	    private Instant lastView; 
+	    private Instant dataArchiviazione; 
+	    private Instant lastUpdate; 
 	    
 	 // aggiungiamo i tag direttamente nel DTO
 	    private List<TagDto> tags;
