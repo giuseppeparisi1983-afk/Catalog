c8e3469148d5ce0332e80aaa9b329b982c100258
giuseppe.parisi@naoslab.it <giuseppe.parisi@naoslab.it>
2026-03-19 11:16:38 +0100

    agginte le query findAll e findById

--- catalog/src/main/java/it/catalog/persistence/repository/DocumentoRepository.java
+++ catalog/src/main/java/it/catalog/persistence/repository/DocumentoRepository.java
@@ -1,7 +1,12 @@
 package it.catalog.persistence.repository;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
@@ -16,6 +21,12 @@ public interface DocumentoRepository extends JpaRepository<Documento, Long>, Jpa
     @Query("SELECT DISTINCT e.stato FROM Documento e")
 	List<String> findDistinctStato();
 
-
+    @EntityGraph(attributePaths = {"tags"}) // <--- Istruisce Hibernate a fare la JOIN solo per questo metodo
+	Page<Documento> findAll(Specification<Documento	> spec, Pageable pageable);
+    
+    @EntityGraph(attributePaths = {"tags"})
+    @Override
+    Optional<Documento> findById(Long id);
+    
 }
 
