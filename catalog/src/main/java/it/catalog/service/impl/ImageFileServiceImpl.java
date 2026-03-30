90ccfc8fdec3fe2fff09895b8998ad184e3ddf27
giuseppe.parisi@naoslab.it <giuseppe.parisi@naoslab.it>
2026-03-30 17:36:08 +0200

    Ristrutturazione del Service che implementa l'interfaccia SearchService

--- catalog/src/main/java/it/catalog/service/impl/ImageFileServiceImpl.java
+++ catalog/src/main/java/it/catalog/service/impl/ImageFileServiceImpl.java
@@ -3,20 +3,21 @@ package it.catalog.service.impl;
 import java.lang.reflect.Field;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import it.catalog.persistence.entity.ImageFile;
 import it.catalog.persistence.repository.ImageRepository;
 import it.catalog.service.dto.ImageDto;
 import it.catalog.service.dto.TagDto;
+import it.catalog.service.dto.search.DtoFilter;
 import it.catalog.service.dto.search.DtoFilter_;
-import it.catalog.service.interfaces.ImageFileService;
+import it.catalog.service.interfaces.SearchService;
 import it.catalog.service.interfaces.TagService;
 import it.catalog.service.mapper.ImageFileMapper;
 import it.catalog.utility.SpecificationFactory;
@@ -24,7 +25,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
-public class ImageFileServiceImpl implements ImageFileService{
+public class ImageFileServiceImpl implements SearchService<ImageDto, DtoFilter>{
 
     private final ImageRepository repo;
     private final ImageFileMapper mapper;
@@ -42,72 +43,47 @@ public class ImageFileServiceImpl implements ImageFileService{
     
 	 @Override
 	 /**query fatta sulla index per avere tutti i tags dell'oggetto immagine */
-  public List<TagDto> getAllTagsForImage() {
+  public List<TagDto> getAllTags() {
 		 return tagService.findByTipoOggetto("Immagini");
   }
     
     @Override
-    public Page<ImageDto> findPage(DtoFilter_ filtro, Pageable pageable, List<String> requiredTags) {
-       
-    	Specification<ImageFile> spec = (root, query, cb) -> cb.conjunction();
+    @Transactional(readOnly = true)
+    public Page<ImageDto> findPage(Pageable pageable,DtoFilter filter) {
     	
-    	if (filtro != null && filtro.getCampo() != null && filtro.getValore() != null) {
-            try {
-           	 Field field = ImageDto.class.getDeclaredField(filtro.getCampo());
-                Class<?> type = field.getType();
-           	 
-           	spec = specFactory.buildOld(
-               filtro.getCampo(),
-               filtro.getValore(),
-               type
-           );
-           	 // 2) Tag filter
-           	if(!requiredTags.isEmpty())
-               spec = spec.and(specFactory.withTag("Immagini",requiredTags));
-               
-            } catch (NoSuchFieldException e) {
-                // Campo non valido, ignora il filtro
-           	 log.error("Campo non valido",e);
-            }
-   	 }
+    	 // 1. Creiamo la specifica basata sul filtro ricevuto dalla UI
+	    Specification<ImageFile> spec = specFactory.build("Immagini", filter);
    	
 	    Page<ImageFile> entityPage =repo.findAll(spec, pageable);
 	    if (entityPage.isEmpty()) {
 	        return new PageImpl<>(Collections.emptyList(), pageable, 0); // caso Not Found
 	    }
 	    
-	    return entityPage.map(entity -> {
-	    	
-	    	ImageDto dto=mapper.toDto(entity);
-	    		
-	    	dto.setTags(tagService.findTagsByObject("Immagini", entity.getId()));
-	    	
-	    	return dto;
-	    		
-	    });
+	    return mapper.toDtoPage(entityPage);
 
     }
+    
+    @Override
+	public long count() {
+
+		return repo.count();		 
+	}
+    
 
     @Override
     /**richiamato dalla pagina del form per aggiunta o modifica di un nuovo item* */
     public ImageDto findById(Long id) {
         var dtoOpt = repo.findById(id).map(mapper::toDto);
-        dtoOpt.ifPresent(dto -> dto.setTags(tagService.findTagsByObject("Immagini", dto.getId())));
         return dtoOpt.orElse(new ImageDto());
     }
 
     @Override
+    @Transactional
     public ImageDto save(ImageDto dto) {
         ImageFile saved = repo.save(mapper.toEntity(dto));
-        // Update tag relations
-        List<String> tagNames=dto.getTags() != null ? dto.getTags().stream() 
-        		.map(TagDto::getNomeTag).collect(Collectors.toList()) : Collections.EMPTY_LIST;
-        
-        tagService.upsertTagsForObject("Immagini", saved.getId(),  tagNames);
-        log.info("Update Tags list for Image file {}",saved.getId());
-        ImageDto res = mapper.toDto(saved);
-        res.setTags(dto.getTags());
-        return res;
+        log.info("Save New Image file. Id {}",saved.getId());
+     
+        return dto;
     }
 
     @Override
