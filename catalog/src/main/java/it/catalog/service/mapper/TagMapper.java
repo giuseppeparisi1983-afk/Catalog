c23ff25add8fb9794292545477ecdee400a4c238
giuseppe.parisi@naoslab.it <giuseppe.parisi@naoslab.it>
2026-05-13 10:05:59 +0200

    Aggiunta del mapping toDto()

--- catalog/src/main/java/it/catalog/service/mapper/TagMapper.java
+++ catalog/src/main/java/it/catalog/service/mapper/TagMapper.java
@@ -1,6 +1,7 @@
 package it.catalog.service.mapper;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.mapstruct.Mapper;
 
@@ -11,7 +12,7 @@ import it.catalog.service.dto.TagDto;
 public interface TagMapper {
 
 	
-	//TagDto toDto(Tag entity); // Questo viene generato automaticamente da MapStruct per la conversione automatica item-to-item
+	TagDto toDto(Tag entity); // Questo viene generato automaticamente da MapStruct per la conversione automatica item-to-item
     Tag toEntity(TagDto dto);
     
     List<TagDto> mapToList(List<Tag> entity); // Questo usa internamente il metodo sopra per ogni item
