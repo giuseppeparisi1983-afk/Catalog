bc366dffa92cee4b760d22057c6ad8ce6c0e123f
giuseppe.parisi@naoslab.it <giuseppe.parisi@naoslab.it>
2026-07-23 18:12:05 +0200

    Eliminata l'annotation @PWA (che serve a rendere l'app installabile su smartphone (come un'app nativa), ma per farlo prende il controllo totale di tutte le icone) e introduzione all'uso delle icone custom

--- catalog/src/main/java/it/catalog/AppShell.java
+++ catalog/src/main/java/it/catalog/AppShell.java
@@ -3,12 +3,20 @@ package it.catalog;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
+import com.vaadin.flow.server.AppShellSettings;
 import com.vaadin.flow.server.PWA;
-import com.vaadin.flow.theme.Theme;
 
 @Push
-@PWA(name = "Catalog App", shortName = "Catalog")
 //@Theme(value = "my-theme")
 @CssImport("./styles/styles.css") // DA VERIFICARE
 public class AppShell implements AppShellConfigurator {
+	
+	
+	@Override
+    public void configurePage(AppShellSettings settings) {
+        // Se l'icona è in src/main/resources/static/favicon.ico
+        // Vaadin la vede come "/favicon.ico"
+        settings.addFavIcon("icon", "favicon.ico", "32x32");
+    }
+	
 }
