08b889a1b9685d0b58a7668b9f9a50b3c8e49e8e
giuseppe.parisi@naoslab.it <giuseppe.parisi@naoslab.it>
2026-08-31 11:14:44 +0200

    Aggiunta dei costruttori.

--- catalog/src/main/java/it/catalog/service/dto/search/StringCriterion.java
+++ catalog/src/main/java/it/catalog/service/dto/search/StringCriterion.java
@@ -1,5 +1,14 @@
 package it.catalog.service.dto.search;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+
+/**Questa classe viene utilizzata per la definizione dei campi di ricerca quando 
+ * il criterio di ricerca è di tipo stringa (es. ricerca per nome o autore.)*/
+
+@AllArgsConstructor
+@NoArgsConstructor
 public class StringCriterion implements SearchCriterion {
 
     private String field; // es. "nome", "cognome"
