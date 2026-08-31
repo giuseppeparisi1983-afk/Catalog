e38664a7285437ca6967ede3fdc35801512499c9
giuseppe.parisi@naoslab.it <giuseppe.parisi@naoslab.it>
2026-08-31 09:55:38 +0200

    Sostituito l'enumeration Difficolta con Livello

--- catalog/src/main/java/it/catalog/persistence/converter/DifficoltaConverter.java
+++ catalog/src/main/java/it/catalog/persistence/converter/DifficoltaConverter.java
@@ -1,15 +1,15 @@
 package it.catalog.persistence.converter;
 
-import it.catalog.common.enums.Difficolta;
+import it.catalog.common.enums.Livello;
 import it.catalog.persistence.converter.base.AbstractLabelConverter;
 import jakarta.persistence.Converter;
 
 //3. Implementazione specifica (Una per ogni Enum)
 @Converter(autoApply = true)
-public class DifficoltaConverter extends AbstractLabelConverter<Difficolta> {
+public class DifficoltaConverter extends AbstractLabelConverter<Livello> {
   
 	public DifficoltaConverter() {
-        super(Difficolta.class);
+        super(Livello.class);
     }
 
 }
