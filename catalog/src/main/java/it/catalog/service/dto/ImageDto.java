678c3e8237b55179ae39694c9bcd96d0d23f68bf
giuseppe.parisi@naoslab.it <giuseppe.parisi@naoslab.it>
2026-08-31 11:24:56 +0200

    ridefinizione del dto con l'implementazione dell'interfaccia HasId e l'introduzione degli enumeration ImageFormat (per gestire l'estensione) e ImageType (per la gestione del tipo di immagine)

--- catalog/src/main/java/it/catalog/service/dto/ImageDto.java
+++ catalog/src/main/java/it/catalog/service/dto/ImageDto.java
@@ -1,26 +1,27 @@
 package it.catalog.service.dto;
 
 import java.time.Instant;
-import java.util.List;
+import java.util.Set;
 
+import it.catalog.common.enums.ImageFormat;
+import it.catalog.common.enums.ImageType;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-public class ImageDto {
+public class ImageDto  implements HasId{
     private Long id;
     private String nome;
-    private String description;
-    private String filename;
-    private String mimeType;
-    private long sizeBytes;
-    private String formato; // JPEG, RAW, TIFF, PNG
-    private String tipoFile; // Fotografia, Sfondo, Illustrazione
+    private String descrizione;
+    private String locandina;
+    private Double dimensione;
+    private ImageFormat formato; // JPEG, RAW, TIFF, PNG
+    private ImageType tipoFile; // Fotografia, Sfondo, Illustrazione
     private boolean cancelled;
     private boolean preferito;
     private String path;
-    private Integer rating;
+    private Double rating;
     private long visualizzazioni;
     private Instant lastView; 
     private Instant dataArchiviazione; 
@@ -29,5 +30,5 @@ public class ImageDto {
     private String note;
 
  // Tags associati private
-    private List<TagDto> tags; // nomi dei tag (solo lettura/scrittura applicativa)
+    private Set<TagDto> tags; // nomi dei tag (solo lettura/scrittura applicativa)
 }
