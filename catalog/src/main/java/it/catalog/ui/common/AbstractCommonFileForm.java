bea346eea15ef80978b1455078dd958a9d351ba7
giuseppe.parisi@naoslab.it <giuseppe.parisi@naoslab.it>
2026-09-02 10:20:14 +0200

    Aggiunta la gestione del menù di navigazione che sarà comune a tutti i form. Ridefinizione del metodo setupCommonBindings(Class<T> beanType) per la gestione del binder per i campi comuni. Si presta attenzione al prefisso distinguento i tipi da video.chitarra Rinomino del metodo setupExtensionCombo(ComboBox<E> combo, E[] values) in setupCombo(ComboBox<E> combo, E[] values) aggiunto il metodo astratto getTagType(); implementato nelle classi figlie per la definizione del tipo di oggetto per i nuovi tag

--- catalog/src/main/java/it/catalog/ui/common/AbstractCommonFileForm.java
+++ catalog/src/main/java/it/catalog/ui/common/AbstractCommonFileForm.java
@@ -4,6 +4,7 @@ import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.function.Supplier;
 
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.HasComponents;
@@ -11,18 +12,21 @@ import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.combobox.MultiSelectComboBox;
 import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.datetimepicker.DateTimePicker;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.NumberField;
 import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.renderer.ComponentRenderer;
 
 import it.catalog.common.enums.FileExtension;
 import it.catalog.service.dto.FilmDto;
 import it.catalog.service.dto.TagDto;
 import it.catalog.service.interfaces.SearchService;
 import it.catalog.ui.utility.AppConverters;
+import it.catalog.ui.utility.BaseFilter;
 import it.catalog.ui.utility.BooleanImageToggle;
 import it.catalog.ui.utility.RatingStarsField;
 import it.catalog.utility.DynamicI18nProvider;
@@ -34,7 +38,8 @@ import it.catalog.utility.DynamicI18nProvider;
  * Questo ci permette di accedere a tutti i metodi del SearchService, incluso getAllTags() per popolare la combo dei TAGS.
  * */
 
-public abstract class AbstractCommonFileForm<T, S extends SearchService<T, ?>> extends AbstractBaseForm<T, S> {
+public abstract class AbstractCommonFileForm<T, S extends SearchService<T, F>, F extends BaseFilter> 
+extends AbstractBaseForm<T, S, F> {
 
     // Campi comuni
     protected TextField nome = new TextField("Nome");
@@ -55,11 +60,11 @@ public abstract class AbstractCommonFileForm<T, S extends SearchService<T, ?>> e
     protected ZoneId fusoOrario = ZoneId.systemDefault();
     protected VerticalLayout mainFormLayout = new VerticalLayout();
 
-    public AbstractCommonFileForm(String title, S service, Class<T> beanType) {
-        super(title, service, beanType);
+    public AbstractCommonFileForm(String title, S service, Class<T> beanType, Supplier<F> filterSupplier, String prefix) {
+        super(title, service, beanType, filterSupplier); 
         
         setupCommonComponents(service);
-        setupCommonBindings(beanType);
+        setupCommonBindings(prefix);
 
     }
 
@@ -69,6 +74,11 @@ public abstract class AbstractCommonFileForm<T, S extends SearchService<T, ?>> e
         mainFormLayout.setPadding(false);
         mainFormLayout.getStyle().set("gap", "8px");
 
+
+        // Definizione della barra di Navigazione in alto
+        addNavigationLayout(mainFormLayout);
+        
+        
         // 1. Gancio per i campi specifici del figlio (che ora non saranno più null!)
         addSpecificTopLayout(mainFormLayout);
         
@@ -83,6 +93,24 @@ public abstract class AbstractCommonFileForm<T, S extends SearchService<T, ?>> e
         add(mainFormLayout);
     }
     
+	/*
+	 * NUOVO METODO HELPER: sposto la creazione della barra di navigazione in un
+	 * metodo separato nella classe Padre, così ogni figlio nel caso debba fare l'override di buildLayout()
+	 * può richiamare questo metodo dove preferisce.
+	 */
+    protected void addNavigationLayout(VerticalLayout container) {
+        HorizontalLayout navBar = new HorizontalLayout(btnPrev, navInfo, btnNext);
+        navBar.setAlignItems(FlexComponent.Alignment.CENTER);
+        navBar.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
+        navBar.setWidthFull();
+//        navBar.getStyle().set("background", "var(--lumo-contrast-5pct)");
+        navBar.getStyle().set("padding", "5px");
+        navBar.getStyle().set("border-radius", "var(--lumo-border-radius-m)");
+        
+        container.add(navBar);
+    }
+    
+    
     
     private void setupCommonComponents(S service) {
         tags.setItems(new ArrayList<>(service.getAllTags()));
@@ -93,6 +121,7 @@ public abstract class AbstractCommonFileForm<T, S extends SearchService<T, ?>> e
             if (nomeTag.isEmpty()) return;
             TagDto newTag = new TagDto();
             newTag.setNomeTag(nomeTag);
+            newTag.setTipoOggetto(getTagType());  // assegniamo il tipo di oggetto specifico per il figlio
             Set<TagDto> current = new HashSet<>(tags.getValue());
             current.add(newTag);
             tags.setValue(current);
@@ -114,44 +143,55 @@ public abstract class AbstractCommonFileForm<T, S extends SearchService<T, ?>> e
         note.setWidth("62%");
     }
 
-    private void setupCommonBindings(Class<T> beanType) {
+    protected void setupCommonBindings(String prefix) {
         
-    	if (!FilmDto.class.isAssignableFrom(beanType))
-    	binder.forField(nome).asRequired("Campo obbligatorio").bind("nome");
+//    	if (!FilmDto.class.isAssignableFrom(beanType))
+    	binder.forField(nome).asRequired("Campo obbligatorio").bind(prefix +"nome");
         
-    	binder.forField(path).withValidator(v -> v != null && (v.contains("\\") || v.contains("/")), "Percorso non valido").asRequired().bind("path");
+    	binder.forField(path).withValidator(v -> v != null && (v.contains("\\") || v.contains("/")), "Percorso non valido").asRequired().bind(prefix +"path");
         
         // Se non è Audio, l'estensione è un TextField comune
 //        if (!AudioDto.class.isAssignableFrom(beanType)) {
 //            binder.forField(estensione).asRequired().bind("estensione");
 //        }
 
-        binder.forField(dimensione).withConverter(new AppConverters.StringToDoubleConverter()).asRequired().bind("dimensione");
-        binder.forField(visualizzazioni).withConverter(new AppConverters.DoubleToLong()).bind("visualizzazioni");
-        binder.forField(tags).bind("tags");
-        binder.forField(preferito).bind("preferito");
-        binder.forField(backup).bind("backup");
-        binder.forField(rating).bind("rating");
+        binder.forField(dimensione).asRequired("Campo obbligatorio").withConverter(new AppConverters.StringToDoubleConverter()).bind(prefix +"dimensione");
+        binder.forField(visualizzazioni).withConverter(new AppConverters.DoubleToLong()).bind(prefix +"visualizzazioni");
+        binder.forField(tags).bind(prefix +"tags");
+        binder.forField(preferito).bind(prefix +"preferito");
+        binder.forField(backup).bind(prefix +"backup");
+        binder.forField(rating).bind(prefix +"rating");
 
         // Date Converters
-        binder.forField(dataArchiviazione).withConverter(new AppConverters.InstantToLocalDate()).asRequired().bind("dataArchiviazione");
-        binder.forField(lastView).withConverter(new AppConverters.InstantToLocalDateTime()).bind("lastView");
-        binder.forField(lastUpdate).withConverter(new AppConverters.InstantToLocalDateTime()).bind("lastUpdate");
+        binder.forField(dataArchiviazione).asRequired("Campo obbligatorio").withConverter(new AppConverters.InstantToLocalDate()).bind(prefix +"dataArchiviazione");
+        binder.forField(lastView).withConverter(new AppConverters.InstantToLocalDateTime()).bind(prefix +"lastView");
+        binder.forField(lastUpdate).withConverter(new AppConverters.InstantToLocalDateTime()).bind(prefix +"lastUpdate");
     }
 
     /**
      * IL METODO CENTRALIZZATO:
      * Configura qualsiasi ComboBox che usi un Enum di tipo FileExtension
      */
-    protected <E extends Enum<E> & FileExtension> void setupExtensionCombo(ComboBox<E> combo, E[] values) {
+    protected <E extends Enum<E> & FileExtension> void setupCombo(ComboBox<E> combo, E[] values) {
         combo.setItems(values);
+        
+        combo.setRenderer(new ComponentRenderer<>(tipo -> {
+    	    Span span = new Span(tipo.getLabel());
+    	    
+    	    // L'attributo HTML "title" attiva il tooltip nativo del browser al passaggio del mouse
+    	    span.getElement().setAttribute("title", tipo.getDescription());
+    	    
+    	    return span;
+    	}));
+    	
         combo.setItemLabelGenerator(FileExtension::getLabel);
         combo.setPlaceholder("Seleziona formato...");
         combo.setClearButtonVisible(true);
-        combo.setWidth("120px"); // Misura standard per tutti
+        combo.setWidth("200px"); // Misura standard per tutti
     }
     
     // Gestisce la riga "Path | Estensione | Dimensione"
+    // Questo metodo viene chiamato dal metodo addSpecificMiddleLayout(VerticalLayout mainLayout) dei figli (Audio, Video, Documento,....) per aggiungere la riga con il path del file, l'estensione specifica e la dimensione.
     protected void addInfoFileLayout(HasComponents container, Component extension) {
     	HorizontalLayout row = new HorizontalLayout(path, extension, dimensione);
         row.setAlignItems(FlexComponent.Alignment.BASELINE);
@@ -181,4 +221,5 @@ public abstract class AbstractCommonFileForm<T, S extends SearchService<T, ?>> e
     // GANCI PER I FIGLI
     protected abstract void addSpecificTopLayout(VerticalLayout mainLayout);
     protected abstract void addSpecificMiddleLayout(VerticalLayout mainLayout);
+    protected abstract String getTagType(); // Definisce il tipo di oggetto per i nuovi tag
 }
